354810099a6063598366185b6cf49188473a64b0
Owusuansah <godfredowusuansah5@gmail.com>
2022-02-23 12:43:17 -0600

    sonarqube

--- sonarqube.sh
+++ sonarqube.sh
@@ -6,14 +6,17 @@
     echo
     su - vagrant
     sudo yum update -y
+    sleep 2
     sudo yum install java-11-openjdk-devel -y
+    sleep 2
     sudo yum install java-11-openjdk-devel -y
+    sleep 2
     cd/opt
     sudo yum install wget -y
+    sleep 2
     sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
     sudo unzip /opt/sonarqube-9.3.0.51899.zip
     sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
     cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
     ./sonar.sh start
     echo "Sonarqube installed and started successfully"
-
